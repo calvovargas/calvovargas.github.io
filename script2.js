21ea21faeffc721ebef80bfd3c55a0e1cb979c33
calvovargas <sebastianandres1120@gmail.com>
2024-09-03 11:40:57 -0500

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,56 @@
+const carrito = document.getElementById('carrito');
+const elemento1 = document.getElementById('lista-1');
+const lista = document.querySelector('#lista-carrito tbody');
+const vaciarCarritoBtn = document.getElementById('vaciar-carrito');
+
+cargarEventlisteners();
+
+function cargarEventlisteners(){
+
+    elemento1.addEventListener('click',comprarElemento);
+    carrito.addEventListener('click', eliminarElemento);
+    vaciarCarritoBtn.addEventListener('click', vaciarCarrito);
+
+}
+
+function comprarElemento(e){
+    e.preventDefault();
+    if(e.target.classList.contains('agregar-carrito')){
+        const elemento = e.target.parentElement.parentElement;
+        leerDatosElemnto(elemento);
+    }
+}
+
+function leerDatosElemnto(elemento){
+    const infoElemento = {
+        imagen: elemento.querySelector('img').src,
+        titulo: elemento.querySelector('h3').textContent,
+        precio: elemento.querySelector('.precio').textContent,
+        id: elemento.querySelector('a').getAttribute('data-id')
+
+    }
+    insertarCarrito(infoElemento);
+}
+
+function insertarCarrito(elemento){
+    const row = document.createElement('tr');
+    row.innerHTML = `
+        <td>
+            <img src="${elemento.imagen}" width=100 >
+        </td>
+
+        <td>
+            ${elemento.titulo}
+        </td>
+
+        <td>
+            ${elemento.precio}
+        </td>
+
+        <td>
+            <a herf="#" class="borrar" data-id="${elemento.id}" >X</a>
+        </td>
+    `;
+    lista.appendChild(row);
+
+}
